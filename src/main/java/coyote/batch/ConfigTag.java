6e0918136a88a77f2e03df430d84602307bc340c
Steve Cote <sdcote@gmail.com>
2015-08-05 11:22:51 -0400

    Support encrypted form of username and password

--- src/main/java/coyote/batch/ConfigTag.java
+++ src/main/java/coyote/batch/ConfigTag.java
@@ -33,6 +33,7 @@ public class ConfigTag {
   public static final String LIMIT = "limit";
   public static final String NAME = "name";
   public static final String PASSWORD = "password";
+  public static final String ENCRYPTED_PASSWORD = "encrypted_password";
   public static final String PRELOAD = "preload";
   public static final String QUERY = "query";
   public static final String SOURCE = "source";
@@ -43,6 +44,7 @@ public class ConfigTag {
   public static final String TYPE = "type";
   /** User name ({@value}) configuration attribute. */
   public static final String USERNAME = "username";
+  public static final String ENCRYPTED_USERNAME = "encrypted_username";
   public static final String DATEFORMAT = "dateformat";
   public static final String ALIGN = "align";
 
