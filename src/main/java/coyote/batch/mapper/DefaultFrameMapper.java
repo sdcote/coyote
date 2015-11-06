dd727f71026b1d3a7effb21bb7fb822b514e0b85
Steve Cote <sdcote@gmail.com>
2015-11-06 07:13:47 -0500

    Comments, logging and javadoc improvements.

--- src/main/java/coyote/batch/mapper/DefaultFrameMapper.java
+++ src/main/java/coyote/batch/mapper/DefaultFrameMapper.java
@@ -14,7 +14,6 @@ package coyote.batch.mapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import coyote.batch.AbstractConfigurableComponent;
 import coyote.batch.ConfigTag;
 import coyote.batch.ConfigurationException;
 import coyote.batch.FrameMapper;
@@ -26,7 +25,8 @@ import coyote.dataframe.DataFrameException;
 
 
 /**
- * 
+ * This is the default mapper which simply copies fields from the source frame 
+ * with one name to the target frame with another.
  */
 public class DefaultFrameMapper extends AbstractFrameMapper implements FrameMapper {
 
@@ -37,13 +37,15 @@ public class DefaultFrameMapper extends AbstractFrameMapper implements FrameMapp
 
 
   /**
+   * Expects a configuration in the form of "Fields" : { "SourceField" : "TargetField" }
+   *  
    * @see coyote.batch.AbstractConfigurableComponent#setConfiguration(coyote.dataframe.DataFrame)
    */
   @Override
   public void setConfiguration( DataFrame frame ) throws ConfigurationException {
     super.setConfiguration( frame );
 
-    // Retrieve the Fields section from the mapper configuration
+    // Retrieve the "fields" section from the configuration frame
     DataFrame mapFrame = null;
     try {
       mapFrame = frame.getAsFrame( ConfigTag.FIELDS );
@@ -51,14 +53,16 @@ public class DefaultFrameMapper extends AbstractFrameMapper implements FrameMapp
       mapFrame = null;
     }
 
+    // If we found the "fields" frame...
     if ( mapFrame != null ) {
+      // For each name:value pair, setup a soure:target mapping
       for ( DataField field : mapFrame.getFields() ) {
         if ( StringUtil.isNotBlank( field.getName() ) && field.getValue().length > 0 ) {
           fieldMap.put( field.getName(), field.getObjectValue().toString() );
         }
       }
     } else {
-      log.warn( "No {} mapping section in Mapper configuration", ConfigTag.FIELDS );
+      log.warn( "No {} section in Mapper configuration", ConfigTag.FIELDS );
     }
 
   }
@@ -73,15 +77,21 @@ public class DefaultFrameMapper extends AbstractFrameMapper implements FrameMapp
   public void process( TransactionContext context ) throws MappingException {
 
     if ( fieldMap.size() > 0 ) {
+      // for each frame in the map (insertion order)
       for ( String workingName : fieldMap.keySet() ) {
-
+        DataField targetField;
         if ( context.getWorkingFrame().contains( workingName ) ) {
-          DataField targetField = (DataField)context.getWorkingFrame().getField( workingName ).clone();
+          // clone the named field from the working frame
+          targetField = (DataField)context.getWorkingFrame().getField( workingName ).clone();
+          // re-name the file to that of the target frame
           targetField.setName( fieldMap.get( workingName ) );
-          context.getTargetFrame().getFields().add( targetField );
         } else {
-          log.warn( "The field '{}' is not in the working frame => {}", workingName, context.getWorkingFrame().toString() );
+          log.debug( "The field '{}' is not in the working frame => {}", workingName, context.getWorkingFrame().toString() );
+          // create a new null frame with the desired name
+          targetField = new DataField( workingName, null );
         }
+        // place the mapped field in the target data frame for writing 
+        context.getTargetFrame().getFields().add( targetField );
       }
     } else {
       // if no field map, just perform a straight clone of the working frame
