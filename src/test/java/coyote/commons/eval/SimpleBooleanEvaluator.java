8ce196cca0d430d90f9f9b3db9010d24a1d99213
Steve Cote <sdcote@gmail.com>
2015-11-30 14:21:03 -0500

    code formatting

--- src/test/java/coyote/commons/eval/SimpleBooleanEvaluator.java
+++ src/test/java/coyote/commons/eval/SimpleBooleanEvaluator.java
@@ -2,52 +2,61 @@ package coyote.commons.eval;
 
 import java.util.Iterator;
 
+
 /** An example of how to implement an evaluator from scratch.
  */
 public class SimpleBooleanEvaluator extends AbstractEvaluator<Boolean> {
   /** The negate unary operator.*/
-  public final static Operator NEGATE = new Operator("!", 1, Operator.Associativity.RIGHT, 3);
+  public final static Operator NEGATE = new Operator( "!", 1, Operator.Associativity.RIGHT, 3 );
   /** The logical AND operator.*/
-  private static final Operator AND = new Operator("&&", 2, Operator.Associativity.LEFT, 2);
+  private static final Operator AND = new Operator( "&&", 2, Operator.Associativity.LEFT, 2 );
   /** The logical OR operator.*/
-  public final static Operator OR = new Operator("||", 2, Operator.Associativity.LEFT, 1);
- 
+  public final static Operator OR = new Operator( "||", 2, Operator.Associativity.LEFT, 1 );
+
   private static final Parameters PARAMETERS;
- 
+
   static {
     // Create the evaluator's parameters
     PARAMETERS = new Parameters();
     // Add the supported operators
-    PARAMETERS.add(AND);
-    PARAMETERS.add(OR);
-    PARAMETERS.add(NEGATE);
+    PARAMETERS.add( AND );
+    PARAMETERS.add( OR );
+    PARAMETERS.add( NEGATE );
   }
- 
+
+
+
+
   public SimpleBooleanEvaluator() {
-    super(PARAMETERS);
+    super( PARAMETERS );
   }
- 
+
+
+
+
   @Override
-  protected Boolean toValue(String literal, Object evaluationContext) {
-    return Boolean.valueOf(literal);
+  protected Boolean toValue( String literal, Object evaluationContext ) {
+    return Boolean.valueOf( literal );
   }
- 
+
+
+
+
   @Override
-  protected Boolean evaluate(Operator operator, Iterator<Boolean> operands, Object evaluationContext) {
-    if (operator == NEGATE) {
+  protected Boolean evaluate( Operator operator, Iterator<Boolean> operands, Object evaluationContext ) {
+    if ( operator == NEGATE ) {
       return !operands.next();
-    } else if (operator == OR) {
+    } else if ( operator == OR ) {
       Boolean o1 = operands.next();
       Boolean o2 = operands.next();
       return o1 || o2;
-    } else if (operator == AND) {
+    } else if ( operator == AND ) {
       Boolean o1 = operands.next();
       Boolean o2 = operands.next();
       return o1 && o2;
     } else {
-      return super.evaluate(operator, operands, evaluationContext);
+      return super.evaluate( operator, operands, evaluationContext );
     }
   }
- 
-  
+
 }
