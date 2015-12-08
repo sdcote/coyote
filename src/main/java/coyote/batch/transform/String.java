5def944fedd77edca7d95cab2b7a01ae80b88537
Steve Cote <sdcote@gmail.com>
2015-12-08 15:22:26 -0500

    Placeholders for basic transforms

---
+++ src/main/java/coyote/batch/transform/String.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and initial implementation
+ */
+package coyote.batch.transform;
+
+import coyote.batch.FrameTransform;
+import coyote.batch.TransformException;
+import coyote.dataframe.DataFrame;
+
+/**
+ * 
+ */
+public class String extends AbstractFrameTransform implements FrameTransform {
+
+  /**
+   * @see coyote.batch.FrameTransform#process(coyote.dataframe.DataFrame)
+   */
+  @Override
+  public DataFrame process( DataFrame frame ) throws TransformException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
