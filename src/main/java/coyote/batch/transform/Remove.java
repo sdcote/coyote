573f4319c0cd822c7c56c98723cb0ebac6d9cf7c
Steve Cote <sdcote@gmail.com>
2015-12-08 16:44:45 -0500

    Initial functionality

--- src/main/java/coyote/batch/transform/Remove.java
+++ src/main/java/coyote/batch/transform/Remove.java
@@ -11,16 +11,22 @@
  */
 package coyote.batch.transform;
 
+import coyote.batch.Batch;
 import coyote.batch.FrameTransform;
 import coyote.batch.TransactionContext;
 import coyote.batch.TransformContext;
 import coyote.batch.TransformException;
+import coyote.batch.eval.EvaluationException;
 import coyote.dataframe.DataFrame;
+import coyote.loader.log.Log;
+import coyote.loader.log.LogMsg;
 
 
 /**
  * If the conditions of this transform are met, then the named field is removed 
  * from the frame. 
+ * 
+ * <p>This only removes the first occurrence of the name field.</p>
  * 
  * <p>Processing continues after this filter is processed.</p>
  */
@@ -33,33 +39,33 @@ public class Remove extends AbstractFrameTransform implements FrameTransform {
   public void open( TransformContext context ) {
     super.open( context );
 
-    // TODO look for the field we are supposed to remove
-    
-    // TODO look for the condition which is to be met
   }
 
 
 
 
- 
   public boolean process( TransactionContext context ) {
-//    // If there is a conditional expression
-//    if ( expression != null ) {
-//
-//      try {
-//        // if the condition evaluates to true
-//        if ( evaluator.evaluateBoolean( expression ) ) {
-//
-//          // TODO: remove the named field
-//
-//          // signal that other filters should still run
-//          return true;
-//
-//        }
-//      } catch ( EvaluationException e ) {
-//        Log.warn( LogMsg.createMsg( Batch.MSG, "Transform.accept_boolean_evaluation_error", e.getMessage() ) );
-//      }
-//    }
+    // If there is a conditional expression
+    if ( expression != null ) {
+
+      try {
+        // if the condition evaluates to true
+        if ( evaluator.evaluateBoolean( expression ) ) {
+
+          // remove the named field
+          if ( context != null ) {
+            if ( context.getWorkingFrame() != null ) {
+              context.getWorkingFrame().remove( fieldName );
+            }
+          } else {
+            Log.warn( LogMsg.createMsg( Batch.MSG, "Transform.context not set" ) );
+          }
+
+        }
+      } catch ( EvaluationException e ) {
+        Log.warn( LogMsg.createMsg( Batch.MSG, "Transform.remove_boolean_evaluation_error", e.getMessage() ) );
+      }
+    }
 
     return true;
   }
