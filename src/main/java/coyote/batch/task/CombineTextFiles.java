af6fa1604ab395f3bdc183a78125679aae527796
Steve Cote <sdcote@gmail.com>
2015-12-11 13:56:07 -0500

    Stub for a task which concatenates multiple text files into one (eg many CSV files into one)

---
+++ src/main/java/coyote/batch/task/CombineTextFiles.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and initial implementation
+ */
+package coyote.batch.task;
+
+import coyote.batch.TaskException;
+import coyote.batch.TransformContext;
+import coyote.batch.TransformTask;
+
+/**
+ * 
+ */
+public class CombineTextFiles extends AbstractFileTask implements TransformTask {
+
+  /**
+   * @see coyote.batch.TransformTask#execute(coyote.batch.TransformContext)
+   */
+  @Override
+  public void execute( TransformContext context ) throws TaskException {
+    // TODO Auto-generated method stub
+
+  }
+
+}
