cfd6f9400283ebca7ce8946c15401cb3d667b152
Steve Cote <sdcote@gmail.com>
2015-12-13 16:15:14 -0500

    finished basic unit test for copy task.

--- src/test/java/coyote/batch/task/CopyTaskTest.java
+++ src/test/java/coyote/batch/task/CopyTaskTest.java
@@ -12,7 +12,6 @@
 package coyote.batch.task;
 
 //import static org.junit.Assert.*;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -42,17 +41,6 @@ public class CopyTaskTest extends AbstractTest {
   private static final TransformContext context = new TransformContext();
 
   private static File testDir = null;
-
-  private static final boolean RECURSE = true;
-  private static final boolean NO_RECURSE = false;
-  private static final boolean PRESERVE = true;
-  private static final boolean NO_PRESERVE = false;
-  private static final boolean KEEPDATE = true;
-  private static final boolean NO_KEEPDATE = false;
-  private static final boolean OVERWRITE = true;
-  private static final boolean NO_OVERWRITE = false;
-  private static final boolean RENAME = true;
-  private static final boolean NO_RENAME = false;
 
 
 
@@ -109,26 +97,54 @@ public class CopyTaskTest extends AbstractTest {
 
 
 
+  /**
+   * Test collecting all the files of a type and make sure they are in their 
+   * respective directories. 
+   */
   @Test
-  public void testDirectory() {
+  public void testRecursePreserve() {
     resetTestDirectory();
-    File fromdir = new File( "src" );
 
-    try {
-      // copy the entire directory
-      FileUtil.copyDirectory( fromdir, testDir, null, RECURSE, PRESERVE, KEEPDATE, OVERWRITE, NO_RENAME );
+    DataFrame cfg = new DataFrame();
+    cfg.put( ConfigTag.FROMDIR, new File( FileUtil.getCurrentWorkingDirectory(), "src" ).getAbsolutePath() );
+    cfg.put( ConfigTag.TODIR, testDir.getAbsolutePath() );
+    cfg.put( ConfigTag.PATTERN, ".*\\.(java)$" );
+    cfg.put( ConfigTag.HALT_ON_ERROR, false );
+    cfg.put( ConfigTag.RECURSE, true );
+    cfg.put( ConfigTag.PRESERVE, true );
+    System.out.println( cfg );
+
+    try (Copy task = new Copy()) {
+
+      // configure the task
+      task.setConfiguration( cfg );
+
+      // initialize it within an operational context
+      task.open( context );
+
+      // execute the task within the context it was opened
+      task.execute();
+
+      assertTrue( testDir.exists() );
+      assertTrue( testDir.isDirectory() );
       File main = new File( testDir, "main" );
       assertTrue( main.exists() );
       assertTrue( main.isDirectory() );
-      File resources = new File( main, "resources" );
-      assertTrue( resources.exists() );
-      assertTrue( resources.isDirectory() );
+      File java = new File( main, "java" );
+      assertTrue( java.exists() );
+      assertTrue( java.isDirectory() );
+      File coyote = new File( java, "coyote" );
+      assertTrue( coyote.exists() );
+      assertTrue( coyote.isDirectory() );
+      File batch = new File( coyote, "batch" );
+      assertTrue( batch.exists() );
+      assertTrue( batch.isDirectory() );
 
-      File msgs = new File( resources, "BatchMsg.properties" );
-      assertTrue( msgs.exists() );
-      assertTrue( msgs.isFile() );
+      File job = new File( batch, "Job.java" );
+      assertTrue( job.exists() );
+      assertTrue( job.isFile() );
 
-    } catch ( IOException e ) {
+    } catch ( ConfigurationException | TaskException | IOException e ) {
       fail( e.getMessage() );
     }
 
@@ -137,12 +153,18 @@ public class CopyTaskTest extends AbstractTest {
 
 
 
+  /**
+   * Test collecting all the files of a type into one directory and make sure 
+   * files with the same name are not over-written, but renamed as the default. 
+   */
   @Test
-  public void test() {
+  public void testDirectoryOverwrite() {
+    resetTestDirectory();
+
     DataFrame cfg = new DataFrame();
     cfg.put( ConfigTag.FROMDIR, new File( FileUtil.getCurrentWorkingDirectory(), "src" ).getAbsolutePath() );
     cfg.put( ConfigTag.TODIR, testDir.getAbsolutePath() );
-    cfg.put( ConfigTag.PATTERN, "([^\\s]+(\\.(?i)(java))$)" );
+    cfg.put( ConfigTag.PATTERN, ".*\\.(java)$" );
     cfg.put( ConfigTag.HALT_ON_ERROR, false );
     cfg.put( ConfigTag.RECURSE, true );
     System.out.println( cfg );
@@ -158,91 +180,6 @@ public class CopyTaskTest extends AbstractTest {
       // execute the task within the context it was opened
       task.execute();
 
-    } catch ( ConfigurationException | TaskException | IOException e ) {
-      fail( e.getMessage() );
-    }
-
-    // now check for the existance of java file in the testdir
-  }
-
-
-
-
-  //@Test
-  public void testDirectoryPattern() {
-    resetTestDirectory();
-
-    File fromdir = new File( "src" );
-
-    String pattern = ".*\\.(java)$";
-
-    try {
-      FileUtil.copyDirectory( fromdir, testDir, pattern, RECURSE, PRESERVE, KEEPDATE, OVERWRITE, NO_RENAME );
-
-      File main = new File( testDir, "main" );
-      assertTrue( main.exists() );
-      assertTrue( main.isDirectory() );
-      File java = new File( main, "java" );
-      assertTrue( java.exists() );
-      assertTrue( java.isDirectory() );
-      File coyote = new File( java, "coyote" );
-      assertTrue( coyote.exists() );
-      assertTrue( coyote.isDirectory() );
-      File loader = new File( coyote, "loader" );
-      assertTrue( loader.exists() );
-      assertTrue( loader.isDirectory() );
-
-      File watchdog = new File( loader, "WatchDog.java" );
-      assertTrue( watchdog.exists() );
-      assertTrue( watchdog.isFile() );
-
-    } catch ( IOException e ) {
-      fail( e.getMessage() );
-    }
-
-  }
-
-
-
-
-  @Test
-  public void testDirectoryOverwrite() {
-    resetTestDirectory();
-
-    File fromdir = new File( "src" );
-
-    String pattern = ".*\\.(java)$";
-
-    try {
-      FileUtil.copyDirectory( fromdir, testDir, pattern, RECURSE, NO_PRESERVE, KEEPDATE, OVERWRITE, RENAME );
-      assertTrue( testDir.exists() );
-      assertTrue( testDir.isDirectory() );
-
-      File info = new File( testDir, "package-info.java" );
-      assertTrue( info.exists() );
-      assertTrue( info.isFile() );
-
-      File info1 = new File( testDir, "package-info.1.java" );
-      assertFalse( info1.exists() );
-
-    } catch ( IOException e ) {
-      fail( e.getMessage() );
-    }
-  }
-
-
-
-
-  @Test
-  public void testDirectoryRename() {
-    resetTestDirectory();
-
-    File fromdir = new File( "src" );
-
-    String pattern = ".*\\.(java)$";
-
-    try {
-      FileUtil.copyDirectory( fromdir, testDir, pattern, RECURSE, NO_PRESERVE, KEEPDATE, NO_OVERWRITE, RENAME );
       assertTrue( testDir.exists() );
       assertTrue( testDir.isDirectory() );
 
@@ -252,11 +189,11 @@ public class CopyTaskTest extends AbstractTest {
 
       File info1 = new File( testDir, "package-info.1.java" );
       assertTrue( info1.exists() );
-      assertTrue( info1.isFile() );
 
-    } catch ( IOException e ) {
+    } catch ( ConfigurationException | TaskException | IOException e ) {
       fail( e.getMessage() );
     }
+
   }
 
 }
