63b8bff184728f84eecdc83e5cad319ac2e7bb25
Steve Cote <sdcote@gmail.com>
2016-02-11 07:48:54 -0500

    No contains link to Service being managed

--- src/main/java/coyote/batch/http/HttpManager.java
+++ src/main/java/coyote/batch/http/HttpManager.java
@@ -14,6 +14,7 @@ package coyote.batch.http;
 import java.io.File;
 import java.io.IOException;
 
+import coyote.batch.Service;
 import coyote.commons.network.http.nugget.GeneralHandler;
 import coyote.commons.network.http.nugget.HTTPDRouter;
 import coyote.commons.network.http.nugget.StaticPageHandler;
@@ -27,14 +28,24 @@ public class HttpManager extends HTTPDRouter {
 
   private static final int PORT = 55290;
 
+  private final Service service;
+
 
 
 
   /**
    * Create the server instance with all the defaults
+   * @param service 
    */
-  public HttpManager() throws IOException {
+  public HttpManager( Service service ) throws IOException {
     super( PORT );
+    if ( service == null )
+      throw new IllegalArgumentException( "Cannot create HttpManager without a Service reference" );
+
+    // Our connection to the service instance we are managing
+    this.service = service;
+
+    // Add the nuggets handling requests to this service
     addMappings();
   }
 
@@ -46,13 +57,14 @@ public class HttpManager extends HTTPDRouter {
    * 
    * <p>Every route is an absolute path. Parameters start with ":". Handler 
    * class should implement {@link UriResponder} interface. If the handler does 
-   * not implement {@link UriResponder} interface - toString() is called.</p>
+   * not implement {@link UriResponder} interface, {@code toString()} is 
+   * called.</p>
    */
   @Override
   public void addMappings() {
     // Set the default mappings
     super.addMappings();
-    
+
     // Add our routes:handlers
     addRoute( "/user", DebugHandler.class );
     addRoute( "/user/:id", DebugHandler.class );
