f484dde9ffc6bb67182e79a53e8dbdc5ca128a50
Steve Cote <sdcote@gmail.com>
2016-02-17 12:24:06 -0500

    Start of a pluggable UI

---
+++ src/main/java/coyote/batch/http/ManagerFactoryBinder.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2016 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and initial implementation
+ */
+package coyote.batch.http;
+
+/**
+ * This represents the interface all StaticManagers must implement so commands 
+ * can be implemented in the HTTP server.
+ */
+public interface ManagerFactoryBinder {
+
+}
