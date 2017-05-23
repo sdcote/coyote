ee3d76cf9cc626cb51ab4b6950f1c69dac86fdec
Steve Cote <sdcote@gmail.com>
2017-05-23 12:49:09 -0400

    optimize imports

--- CoyoteMQ/src/test/java/coyote/mq/RabbitReaderTest.java
+++ CoyoteMQ/src/test/java/coyote/mq/RabbitReaderTest.java
@@ -22,7 +22,6 @@ import java.util.List;
 import org.junit.Test;
 
 import coyote.dataframe.DataFrame;
-import coyote.dataframe.marshal.JSONMarshaler;
 import coyote.dx.ConfigTag;
 import coyote.dx.FrameReader;
 import coyote.dx.context.TransactionContext;
