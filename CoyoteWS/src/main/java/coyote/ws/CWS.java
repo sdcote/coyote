ddf4dfb95211dea9409e438d67e9429ae78230a9
Steve Cote <sdcote@gmail.com>
2017-06-18 11:48:19 -0400

    Added fixture for version tracking

---
+++ CoyoteWS/src/main/java/coyote/ws/CWS.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and implementation
+ */
+package coyote.ws;
+
+import coyote.commons.Version;
+import coyote.loader.log.LogMsg.BundleBaseName;
+
+
+/**
+ * 
+ */
+public class CWS {
+
+  public static final Version VERSION = new Version( 0, 1, 1, Version.EXPERIMENTAL );
+  public static final String NAME = "CWS";
+
+  public static final BundleBaseName MSG;
+  static {
+    MSG = new BundleBaseName( "CwsMsg" );
+  }
+
+}
