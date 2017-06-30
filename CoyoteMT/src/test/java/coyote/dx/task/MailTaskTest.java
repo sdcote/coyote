e3886b8928a7f969d744b913c3231a03980e0f29
Steve Cote <sdcote@gmail.com>
2017-06-30 14:31:05 -0400

    Mail task now supports conditional execution via the baseclass

--- CoyoteMT/src/test/java/coyote/dx/task/MailTaskTest.java
+++ CoyoteMT/src/test/java/coyote/dx/task/MailTaskTest.java
@@ -70,14 +70,51 @@ public class MailTaskTest {
     try (Mail task = new Mail()) {
       task.setConfiguration( cfg );
       task.open( context );
-//      task.execute();
+      //      task.execute();
 
       assertTrue( context.isNotInError() );
 
     } catch ( ConfigurationException | IOException e ) {
       fail( e.getMessage() );
-//    } catch ( TaskException e ) {
-//      fail( e.getMessage() );
+      //    } catch ( TaskException e ) {
+      //      fail( e.getMessage() );
+    }
+  }
+
+
+
+
+  @Test
+  public void conditional() {
+    Config cfg = new Config();
+    //cfg.put( ConfigTag.CONDITION, "contextError && equals(currentRow,0)" ); // errors and no data
+    //cfg.put( ConfigTag.CONDITION, "contextError && noRowsProcessed" ); // errors and no data
+    //cfg.put( ConfigTag.CONDITION, "! contextError && noRowsProcessed" ); // no errors and no data
+    //cfg.put( ConfigTag.CONDITION, "contextError || noRowsProcessed" ); // no errors or no data
+    cfg.put( ConfigTag.CONDITION, "contextError" ); // errors occurred
+    cfg.put( ConfigTag.HOST, "smtp.gmail.com" );
+    cfg.put( ConfigTag.PORT, 587 );
+    cfg.put( ConfigTag.PROTOCOL, "GmailTLS" );
+    cfg.put( Loader.ENCRYPT_PREFIX + ConfigTag.USERNAME, "q3YEnVqgPxdNauVIKi/0CNOIA0xDP7rvBNyXksa6eYCrKc9OGmZ2yWl+Yrrw9RNW3QpXrX0Aluw=" );
+    cfg.put( Loader.ENCRYPT_PREFIX + ConfigTag.PASSWORD, "nrasR4FXrf+ZyM1cigtHktGWc+UeZ5EPoNj/Lack6tXPgx58hFwwXq7BiYmN5SfA" );
+    cfg.put( Loader.ENCRYPT_PREFIX + CMT.SENDER, "q3YEnVqgPxdNauVIKi/0CNOIA0xDP7rvBNyXksa6eYCrKc9OGmZ2yWl+Yrrw9RNW3QpXrX0Aluw=" );
+    cfg.put( Loader.ENCRYPT_PREFIX + CMT.RECEIVER, "Za1c7XyXOAxh2u35/7iI4l6UEbOUxmRvXHpf9eGrUQfwZRQzKybKgQ==" );
+    cfg.put( CMT.SUBJECT, "Mail Task Test" );
+    cfg.put( CMT.ATTACH, new File( FileUtil.getCurrentWorkingDirectory(), "README.md" ).getAbsolutePath() );
+    cfg.put( CMT.BODY, "[#$receiver#]:\n\nFind attached the results of the request job run.\n\nRegards,\n[#$sender#]" );
+    Log.info( cfg.toFormattedString() );
+
+    try (Mail task = new Mail()) {
+      task.setConfiguration( cfg );
+      task.open( context );
+      task.execute();
+
+      assertTrue( context.isNotInError() );
+
+    } catch ( ConfigurationException | IOException e ) {
+      fail( e.getMessage() );
+    } catch ( TaskException e ) {
+      fail( e.getMessage() );
     }
   }
 
