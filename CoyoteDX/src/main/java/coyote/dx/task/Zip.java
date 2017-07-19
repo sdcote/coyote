324f55b00ef8988dff825dfabffa81092bbbca64
Steve Cote <sdcote@gmail.com>
2017-07-19 17:09:06 -0400

    Created a synonym/alias for the Archive task

---
+++ CoyoteDX/src/main/java/coyote/dx/task/Zip.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2017 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and implementation
+ */
+package coyote.dx.task;
+
+/**
+ * This task is an alias/synonym for the Archive task
+ */
+public class Zip extends Archive {
+
+}
