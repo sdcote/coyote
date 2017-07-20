d4bb77f2698d155ee26492a9af20605a9bd88d41
Steve Cote <sdcote@gmail.com>
2017-07-20 08:01:03 -0400

    Start on CheckSize

---
+++ CoyoteDX/src/main/java/coyote/dx/task/CheckSize.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and implementation
+ */
+package coyote.dx.task;
+
+import java.io.File;
+
+import coyote.commons.StringUtil;
+import coyote.dx.CDX;
+import coyote.dx.ConfigTag;
+import coyote.dx.TaskException;
+import coyote.loader.log.Log;
+import coyote.loader.log.LogMsg;
+
+
+/**
+ * Check the size of the given file.
+ */
+public class CheckSize extends AbstractFileTask {
+
+  /**
+   * @see coyote.dx.task.AbstractTransformTask#performTask()
+   */
+  @Override
+  protected void performTask() throws TaskException {
+    final String source = getSourceOrFile();
+    if ( StringUtil.isNotBlank( source ) ) {
+      Log.debug( getClass().getSimpleName() + " using a filename of '" + source + "'" );;
+      final File file = getExistingFile( source );
+      Log.debug( getClass().getSimpleName() + " using absolute filename of '" + file.getAbsolutePath() + "'" );;
+
+      if ( file.exists() ) {
+
+        // TODO: use the given attribute name in the context
+        String size = getContext().getAsString( ConfigTag.CONTEXT );
+        Log.info( "Size should be "+size );
+        
+        if ( file.length() > 0 ) {
+
+          Log.fatal( getClass().getSimpleName() + ": Not fully implemented" );
+
+        } else {
+          Log.warn( LogMsg.createMsg( CDX.MSG, "%s did not read any data from %s - empty file (%s)", getClass().getSimpleName(), source, file.getAbsolutePath() ) );
+        }
+      } else {
+        final String msg = LogMsg.createMsg( CDX.MSG, "%s failed: File %s does not exist (%s)", getClass().getSimpleName(), source, file.getAbsolutePath() ).toString();
+        Log.error( msg );
+        if ( haltOnError ) {
+          getContext().setError( msg );
+          return;
+        }
+      }
+    } else {
+      final String msg = LogMsg.createMsg( CDX.MSG, "%s failed: No data in %s configuration attribute", getClass().getSimpleName(), ConfigTag.SOURCE ).toString();
+      Log.error( msg );
+      if ( haltOnError ) {
+        getContext().setError( msg );
+        return;
+      }
+    }
+  }
+
+}
