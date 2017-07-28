84c5d83e9d60dedc9925581ecc02ad4e26df43d9
Steve Cote <sdcote@gmail.com>
2017-07-28 17:26:36 -0400

    More complete test of RunJob by using a job which retrieves data from the web and does not rely on local data file

--- CoyoteDX/src/test/java/coyote/dx/task/RunJobTest.java
+++ CoyoteDX/src/test/java/coyote/dx/task/RunJobTest.java
@@ -12,16 +12,21 @@
 package coyote.dx.task;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import coyote.dataframe.DataFrame;
 import coyote.dx.ConfigTag;
 import coyote.dx.TaskException;
 import coyote.dx.context.TransformContext;
+import coyote.dx.writer.ContextWriter;
 import coyote.loader.cfg.Config;
 import coyote.loader.cfg.ConfigurationException;
 import coyote.loader.log.ConsoleAppender;
@@ -38,7 +43,8 @@ public class RunJobTest {
    */
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
-    Log.addLogger( Log.DEFAULT_LOGGER_NAME, new ConsoleAppender( Log.TRACE_EVENTS | Log.DEBUG_EVENTS | Log.INFO_EVENTS | Log.WARN_EVENTS | Log.ERROR_EVENTS | Log.FATAL_EVENTS ) );
+    Log.addLogger( Log.DEFAULT_LOGGER_NAME, new ConsoleAppender( Log.INFO_EVENTS | Log.WARN_EVENTS | Log.ERROR_EVENTS | Log.FATAL_EVENTS ) );
+    // Log.addLogger( Log.DEFAULT_LOGGER_NAME, new ConsoleAppender( Log.TRACE_EVENTS | Log.DEBUG_EVENTS | Log.INFO_EVENTS | Log.WARN_EVENTS | Log.ERROR_EVENTS | Log.FATAL_EVENTS ) );
   }
 
 
@@ -58,14 +64,30 @@ public class RunJobTest {
     final TransformContext context = new TransformContext();
 
     Config cfg = new Config();
-    cfg.put( ConfigTag.FILE, "simple" );
-    //System.out.println( cfg );
+    cfg.put( ConfigTag.FILE, "src/resources/demo/BitcoinPrice" );
+    Log.debug("\"RunJob\":"+ cfg );
 
     try (RunJob task = new RunJob()) {
       task.setConfiguration( cfg );
       task.open( context );
-     // task.execute();
+      task.execute();
       assertFalse( context.getErrorMessage(), context.isInError() );
+
+      // The context of jobs are stored under the name of the job
+      Object results = context.get("BitcoinPrice");
+      assertNotNull(results);
+      assertTrue( results instanceof Map); // contexts are maps
+      
+      // The context writer writes all its frames to an array stored in its 
+      // context with a default key of ContextOutput 
+      Object object = ((Map)results).get(ContextWriter.DEFAULT_CONTEXT_FIELD);
+      assertNotNull(object);
+      DataFrame[] frames = (DataFrame[])object; // should be an array of frames
+      assertTrue( frames.length>0);
+      
+      DataFrame frame = frames[0]; // get the first one
+      assertNotNull(frame);
+      System.out.println( frame );
     }
 
   }
