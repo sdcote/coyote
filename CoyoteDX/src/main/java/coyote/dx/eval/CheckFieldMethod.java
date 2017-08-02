a6557fbe32c48d8761fbee8563ef0b8741650f37
Steve Cote <sdcote@gmail.com>
2017-08-02 17:24:00 -0400

    Added tests for different fields of the transaction context

--- CoyoteDX/src/main/java/coyote/dx/eval/CheckFieldMethod.java
+++ CoyoteDX/src/main/java/coyote/dx/eval/CheckFieldMethod.java
@@ -7,33 +7,120 @@
  */
 package coyote.dx.eval;
 
-import coyote.commons.StringUtil;
 import coyote.dx.context.TransformContext;
 
 
 /**
  * Checks if the field contains expected values.
+ * 
+ * <p>There are 6 supported operators:<ul>
+ * <li>EQ (==) - equal (equivalent) to
+ * <li>EI - equal (equivalent) to ignores case
+ * <li>LT (&lt;) - Less Than
+ * <li>LE (&lt;=) - Less than or Equal to
+ * <li>GT (&gt;) - Greater Than
+ * <li>GE (&gt;=) - Greater Than or Equal to
+ * <li>NE (!=) - Not equal to
+ * </ul>
+ * 
+ * <p>String values in the context will be parsed into numeric values with any 
+ * parsing errors leaving the value as a string and the test probably failing.
+ * 
+ * <p>Dates are converted to longs representing epoch times.
+ * 
+ * <p>Booleans are converted to 0 (false) and 1 (true) for comparison.
+ * 
+ * <p>EI (equal to ignoring case) causes values to be converted to strings for 
+ * comparison. Numerics are not formatted in any way and will not contain 
+ * group separators.
  */
 public class CheckFieldMethod extends AbstractBooleanMethod {
 
   /**
-   * Resolve the token in the context and determine if it is null or an empty 
-   * string ("").
-   * 
+   * Resolve the named field in the context and determine if it satisfies the 
+   * operator and comparison value.
+   *
    * @param context The transform context in which to look for the job status
    * @param field name of the value to resolve in the context.
    * @param operator token representing the comparative operation to perform
    * @param expected the value against which the field is evaluated.
-   * 
+   *
    * @return true if the named field matches expected comparison, false 
    *         otherwise.
+   *
+   * @throws IllegalArgumentException if the operator is not supported.
    */
-  public static Boolean execute(TransformContext context, String field, String operator, String expected) {
+  public static Boolean execute(TransformContext context, String field, String operator, String expected) throws IllegalArgumentException {
     String key = sanitize(field);
-    String value = context.resolveToString(key);
-    
-    
+    Object obj = context.resolveToValue(field);
+
+    Operator op = Operator.getOperator(operator);
+    if (Operator.EQ.equals(op)) {
+      System.out.println("equals");
+    } else if (Operator.EI.equals(op)) {
+      System.out.println("equals, ignore case");
+    } else if (Operator.LT.equals(op)) {
+      System.out.println("equals");
+    } else if (Operator.LE.equals(op)) {
+      System.out.println("equals");
+    } else if (Operator.GT.equals(op)) {
+      System.out.println("equals");
+    } else if (Operator.GE.equals(op)) {
+      System.out.println("equal");
+    } else if (Operator.NE.equals(op)) {
+      System.out.println("not equals");
+    }
+
     return true;
+  }
+
+  /**
+   * Supported comparison operators
+   */
+  public enum Operator {
+    EQ, LT, LE, GT, GE, NE, EI;
+
+    /**
+     * Convert text into an operator.
+     * 
+     * @param token the text to parse
+     * 
+     * @return the operator represented by the text
+     * 
+     * @throws IllegalArgumentException if the text could not be parsed into 
+     *         an operator.
+     */
+    public static Operator getOperator(String token) throws IllegalArgumentException {
+      if (token != null) {
+        String value = token.trim().toUpperCase();
+        switch (value) {
+          case "EQ":
+          case "==":
+            return EQ;
+          case "EI":
+            return EI;
+          case "LT":
+          case "<":
+            return EQ;
+          case "LE":
+          case "<=":
+            return EQ;
+          case "GT":
+          case ">":
+            return EQ;
+          case "GE":
+          case ">=":
+            return EQ;
+          case "NE":
+          case "!=":
+            return EQ;
+          default:
+            throw new IllegalArgumentException("Unsupported operator '" + token + "'");
+        }
+      } else {
+        throw new IllegalArgumentException("No operator");
+      }
+    }
   }
 
 }
--- CoyoteDX/src/test/java/coyote/dx/eval/CheckFieldMethodTest.java
+++ CoyoteDX/src/test/java/coyote/dx/eval/CheckFieldMethodTest.java
@@ -7,24 +7,26 @@
  */
 package coyote.dx.eval;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import coyote.dx.Symbols;
+import coyote.dataframe.DataFrame;
+import coyote.dx.context.TransactionContext;
 import coyote.dx.context.TransformContext;
 
+
 /**
- * 
+ * Unit tests for the checkField method.
  */
 public class CheckFieldMethodTest {
 
   private static Evaluator evaluator = null;
-   private static TransformContext transformContext = null;
+  private static TransformContext transformContext = null;
+  private static TransactionContext context = null;
 
 
 
@@ -34,21 +36,114 @@ public class CheckFieldMethodTest {
    */
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
-
     transformContext = new TransformContext();
+    transformContext.set("one", 1);
+    transformContext.set("two", 2);
+    transformContext.set("name", "Bob");
+    
+    context = new TransactionContext(transformContext);
+    context.setLastFrame(true);
+    context.setSourceFrame(new DataFrame().set("field1", "value1").set("Field2", "Value2").set("BooleanField", true));
+    context.setWorkingFrame(new DataFrame().set("field3", "value3").set("Field4", "Value4").set("DateField", new Date()));
+    context.setTargetFrame(new DataFrame().set("field5", "value5").set("Field6", "Value6").set("LongField", 123L));
+    transformContext.setTransaction(context);
 
     evaluator = new Evaluator(transformContext);
-
   }
 
 
 
+
   @Test
-  public void test() {
+  public void evaluatorCheck() {
     String expression;
 
     expression = "checkField(Test, LT, 123)";
-    assertTrue(evaluator.evaluateBoolean(expression));    
+    assertTrue(evaluator.evaluateBoolean(expression));
+
+    expression = "checkField(Working.field3, LT, 123)";
+    assertTrue(evaluator.evaluateBoolean(expression));
+
+    expression = "checkField(Working.fieldX, LT, 123)";
+    assertTrue(evaluator.evaluateBoolean(expression));
+
+    expression = "checkField(Source.field1, LT, 123)";
+    assertTrue(evaluator.evaluateBoolean(expression));
+
+    expression = "checkField(Source.fieldX, LT, 123)";
+    assertTrue(evaluator.evaluateBoolean(expression));
+
+    expression = "checkField(Target.field5, LT, 123)";
+    assertTrue(evaluator.evaluateBoolean(expression));
+
+    expression = "checkField(Target.fieldX, LT, 123)";
+    assertTrue(evaluator.evaluateBoolean(expression));
+}
+
+
+
+
+  @Test
+  public void equalTo() {
+    CheckFieldMethod.execute(transformContext, "name", "EQ", "Bob");
+    CheckFieldMethod.execute(transformContext, "name", CheckFieldMethod.Operator.EQ.toString(), "Bob");
+    CheckFieldMethod.execute(transformContext, "name", "EQ", "Robert");
+    CheckFieldMethod.execute(transformContext, "name", CheckFieldMethod.Operator.EQ.toString(), "Robert");
+  }
+
+
+
+
+  @Test
+  public void equalToIgnoreCase() {
+    CheckFieldMethod.execute(transformContext, "name", "EI", "BOB");
+    CheckFieldMethod.execute(transformContext, "name", CheckFieldMethod.Operator.EI.toString(), "BOB");
+    CheckFieldMethod.execute(transformContext, "name", "EQ", "Robert");
+    CheckFieldMethod.execute(transformContext, "name", CheckFieldMethod.Operator.EQ.toString(), "Robert");
+  }
+
+
+
+
+  @Test
+  public void lessThan() {
+    CheckFieldMethod.execute(transformContext, "one", "LT", "2");
+    CheckFieldMethod.execute(transformContext, "one", CheckFieldMethod.Operator.LT.toString(), "2");
+  }
+
+
+
+
+  @Test
+  public void lessThanEqualTo() {
+    CheckFieldMethod.execute(transformContext, "one", "LE", "2");
+    CheckFieldMethod.execute(transformContext, "one", CheckFieldMethod.Operator.LE.toString(), "2");
+    CheckFieldMethod.execute(transformContext, "two", "LE", "2");
+    CheckFieldMethod.execute(transformContext, "two", CheckFieldMethod.Operator.LE.toString(), "2");
+  }
+
+
+
+
+  @Test
+  public void greaterThan() {
+    CheckFieldMethod.execute(transformContext, "two", "GT", "1");
+    CheckFieldMethod.execute(transformContext, "two", CheckFieldMethod.Operator.GT.toString(), "1");
+    CheckFieldMethod.execute(transformContext, "one", "GT", "1");
+    CheckFieldMethod.execute(transformContext, "one", CheckFieldMethod.Operator.GT.toString(), "1");
+  }
+
+
+
+
+  @Test
+  public void greaterThanEqualTo() {
+    CheckFieldMethod.execute(transformContext, "two", "GE", "1");
+    CheckFieldMethod.execute(transformContext, "two", CheckFieldMethod.Operator.GE.toString(), "1");
+    CheckFieldMethod.execute(transformContext, "two", "GE", "2");
+    CheckFieldMethod.execute(transformContext, "two", CheckFieldMethod.Operator.GE.toString(), "2");
+    CheckFieldMethod.execute(transformContext, "two", "GE", "3");
+    CheckFieldMethod.execute(transformContext, "two", CheckFieldMethod.Operator.GE.toString(), "3");
   }
 
 }
