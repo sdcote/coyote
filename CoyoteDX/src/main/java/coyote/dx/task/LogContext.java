ae1abae03d37720758906676294ff14b0087a937
Steve Cote <sdcote@gmail.com>
2017-08-05 12:32:30 -0400

    Log category is now configurable

--- CoyoteDX/src/main/java/coyote/dx/task/LogContext.java
+++ CoyoteDX/src/main/java/coyote/dx/task/LogContext.java
@@ -4,19 +4,22 @@
  * This program and the accompanying materials are made available under the 
  * terms of the MIT License which accompanies this distribution, and is 
  * available at http://creativecommons.org/licenses/MIT/
- *
- * Contributors:
- *   Stephan D. Cote 
- *      - Initial concept and implementation
  */
 package coyote.dx.task;
 
+import java.util.Locale;
+
+import coyote.commons.StringUtil;
+import coyote.dx.ConfigTag;
 import coyote.dx.TaskException;
 import coyote.loader.log.Log;
 
 
 /**
+ * Log the current state of the context including the symbol table.
  * 
+ * <p>By default it logs it to the INFO category, but can be configured 
+ * thusly:<pre>"LogContext":{"category":"debug"}</pre>
  */
 public class LogContext extends AbstractFileTask {
 
@@ -25,7 +28,14 @@ public class LogContext extends AbstractFileTask {
    */
   @Override
   protected void performTask() throws TaskException {
-    Log.info( getContext().dump() );
+    String category = getString(ConfigTag.CATEGORY);
+    long mask;
+    if (StringUtil.isNotEmpty(category)) {
+      mask = Log.getCode(category.trim().toUpperCase(Locale.getDefault()));
+    } else {
+      mask = Log.INFO_EVENTS;
+    }
+    Log.append(mask, getContext().dump());
   }
 
 }
