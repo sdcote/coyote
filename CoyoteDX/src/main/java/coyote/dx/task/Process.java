2b1edd4f19cc7b7b6fc548c80738eb74f189f529
Steve Cote <sdcote@gmail.com>
2017-08-15 11:53:25 -0400

    Process task fully functional

--- CoyoteDX/src/main/java/coyote/dx/task/Process.java
+++ CoyoteDX/src/main/java/coyote/dx/task/Process.java
@@ -7,12 +7,98 @@
  */
 package coyote.dx.task;
 
+import coyote.commons.CommandLineProcess;
+import coyote.commons.CommandLineProcess.Result;
+import coyote.commons.StringUtil;
+import coyote.commons.template.Template;
+import coyote.dx.ConfigTag;
+import coyote.dx.TaskException;
+import coyote.loader.log.Log;
+
+
 /**
  * Runs a process on the host operating system.
  * 
- * <p>This can be a script or other programm and the command line is templated 
+ * <p>This can be a script or other program and the command line is templated 
  * so all the symbols can be used in the invocation.
  */
 public class Process extends AbstractTransformTask {
+
+  protected static final String COMMAND = "Command";
+  protected static final String EXIT_CODE = "ExitCode";
+  protected static final String START = "Start";
+  protected static final String DURATION = "Duration";
+  protected static final String OUTPUT = "Output";
+  protected static final String ERROR = "Error";
+  private static final String DELIMITER = ".";
+
+
+
+
+  /**
+  * @see coyote.dx.task.AbstractTransformTask#performTask()
+  */
+  @Override
+  protected void performTask() throws TaskException {
+
+    final String command = getString(COMMAND);
+    final String name = getString(ConfigTag.NAME);
+    final String commandLine = Template.resolve(command, getContext().getSymbols());
+
+    if (StringUtil.isNotBlank(command)) {
+      Result result = CommandLineProcess.exec(commandLine);
+
+      if (Log.isLogging(Log.DEBUG_EVENTS)) {
+        Log.debug("Executed '" + commandLine + "' - exit code: " + result.getExitCode() + "  " + result.getDuration() + "ms");
+        StringBuffer b = new StringBuffer("STDOUT:\n");
+        String[] output = result.getOutput();
+        for (int x = 0; x < output.length; x++) {
+          b.append(output[x]);
+          if (x + 1 < output.length) {
+            b.append("\n");
+          }
+        }
+        Log.debug(b);
+      }
+
+      String contextKey;
+      if (StringUtil.isBlank(name)) {
+        contextKey = command.split("\\s+")[0];
+      } else {
+        contextKey = name;
+      }
+      getContext().set(contextKey + DELIMITER + EXIT_CODE, result.getExitCode());
+      getContext().set(contextKey + DELIMITER + DURATION, result.getDuration());
+      getContext().set(contextKey + DELIMITER + START, result.getStartTime());
+      getContext().set(contextKey + DELIMITER + COMMAND, result.getCommand());
+      getContext().set(contextKey + DELIMITER + OUTPUT, result.getOutput());
+      getContext().set(contextKey + DELIMITER + ERROR, result.getError());
+
+      if (result.getExitCode() != 0) {
+        StringBuffer b = new StringBuffer("Error running '");
+        b.append(commandLine);
+        b.append("'");
+        String[] errors = result.getError();
+        if (errors.length > 0) {
+          b.append(" STDERR:\n");
+          for (int x = 0; x < errors.length; x++) {
+            b.append(errors[x]);
+            b.append("\n");
+          }
+        } else {
+          b.append(", (no data on STDERR)");
+        }
+
+        if (haltOnError) {
+          throw new TaskException(b.toString());
+        } else {
+          Log.error(b);
+          return;
+        }
+      }
+    } else {
+      Log.notice("Process had no command to execute.");
+    }
+  }
 
 }
---
+++ CoyoteDX/src/test/java/coyote/dx/task/ProcessTest.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2017 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ */
+package coyote.dx.task;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import coyote.dx.ConfigTag;
+import coyote.dx.TaskException;
+import coyote.dx.context.TransformContext;
+import coyote.loader.cfg.Config;
+import coyote.loader.cfg.ConfigurationException;
+import coyote.loader.log.ConsoleAppender;
+import coyote.loader.log.Log;
+
+
+/**
+ * 
+ */
+public class ProcessTest {
+
+  /**
+   * @see coyote.dx.task.AbstractTaskTest#setUpBeforeClass()
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    Log.addLogger(Log.DEFAULT_LOGGER_NAME, new ConsoleAppender());
+  }
+
+
+
+
+  @Test
+  public void execute() throws IOException, ConfigurationException, TaskException {
+    final TransformContext context = new TransformContext();
+    final String cmd = "arp -a";
+    final Config cfg = new Config();
+    cfg.put(Process.COMMAND, cmd);
+    Log.info("\"Process\":" + cfg);
+
+    try (Process task = new Process()) {
+      task.setConfiguration(cfg);
+      task.open(context);
+      task.execute();
+      assertFalse(context.getErrorMessage(), context.isInError());
+
+      // The context of jobs are stored under the name of the job or the name 
+      // of the command being called if name is not specified
+      final Object exitCode = context.get("arp." + Process.EXIT_CODE);
+      assertNotNull(exitCode);
+      assertTrue(exitCode instanceof Integer);
+      assertTrue(0 == ((Integer)exitCode).intValue());
+
+      final Object duration = context.get("arp." + Process.DURATION);
+      assertNotNull(duration);
+      assertTrue(duration instanceof Long);
+
+      final Object start = context.get("arp." + Process.START);
+      assertNotNull(start);
+      assertTrue(start instanceof Long);
+      assertTrue((Long)start > 0);
+
+      final Object command = context.get("arp." + Process.COMMAND);
+      assertNotNull(command);
+      assertTrue(cmd instanceof String);
+      assertEquals(cmd, (String)command);
+
+      final Object output = context.get("arp." + Process.OUTPUT);
+      assertNotNull(output);
+      assertTrue(output instanceof String[]);
+      //      String[] array = (String[])output;
+      //      for (int x = 0; x < array.length; x++) {
+      //        System.out.println(array[x]);
+      //      }
+
+      final Object error = context.get("arp." + Process.ERROR);
+      assertNotNull(error);
+      assertTrue(error instanceof String[]);
+      //      array = (String[])error;
+      //      for (int x = 0; x < array.length; x++) {
+      //        System.out.println(array[x]);
+      //      }
+    }
+  }
+
+
+
+
+  @Test
+  public void executeNamed() throws IOException, ConfigurationException, TaskException {
+    final TransformContext context = new TransformContext();
+
+    final Config cfg = new Config();
+    cfg.put(Process.COMMAND, "arp -a");
+    cfg.put(ConfigTag.NAME, "ProcessTest"); // override the name of the process
+    Log.info("\"Process\":" + cfg);
+
+    try (Process task = new Process()) {
+      task.setConfiguration(cfg);
+      task.open(context);
+      task.execute();
+      assertFalse(context.getErrorMessage(), context.isInError());
+
+      final Object exitCode = context.get("ProcessTest." + Process.EXIT_CODE);
+      assertNotNull(exitCode);
+
+      final Object duration = context.get("ProcessTest." + Process.DURATION);
+      assertNotNull(duration);
+
+      final Object output = context.get("ProcessTest." + Process.OUTPUT);
+      assertNotNull(output);
+
+      final Object error = context.get("ProcessTest." + Process.ERROR);
+      assertNotNull(error);
+    }
+  }
+
+
+
+
+  @Test
+  public void invalidCommand() throws IOException, ConfigurationException, TaskException {
+    final TransformContext context = new TransformContext();
+    Log.startLogging(Log.DEBUG);
+
+    final Config cfg = new Config();
+    cfg.put(Process.COMMAND, "foo -bar -baz");
+    cfg.put(ConfigTag.HALT_ON_ERROR, false);
+    Log.info("\"Process\":" + cfg);
+
+    try (Process task = new Process()) {
+      task.setConfiguration(cfg);
+      task.open(context);
+      task.execute();
+      assertFalse(context.getErrorMessage(), context.isInError());
+
+      final Object exitCode = context.get("foo." + Process.EXIT_CODE);
+      assertNotNull(exitCode);
+      assertTrue(exitCode instanceof Integer);
+      assertTrue(-1 == ((Integer)exitCode).intValue());
+
+      final Object duration = context.get("foo." + Process.DURATION);
+      assertNotNull(duration);
+
+      final Object output = context.get("foo." + Process.OUTPUT);
+      assertNotNull(output);
+
+      final Object error = context.get("foo." + Process.ERROR);
+      assertNotNull(error);
+    }
+    Log.stopLogging(Log.DEBUG);
+  }
+
+
+
+  @Test
+  public void badParameter() throws IOException, ConfigurationException, TaskException {
+    final TransformContext context = new TransformContext();
+
+    final Config cfg = new Config();
+    cfg.put(Process.COMMAND, "arp -bar -baz");
+    cfg.put(ConfigTag.HALT_ON_ERROR, false);
+    Log.info("\"Process\":" + cfg);
+
+    try (Process task = new Process()) {
+      task.setConfiguration(cfg);
+      task.open(context);
+      task.execute();
+      assertFalse(context.getErrorMessage(), context.isInError());
+
+      final Object exitCode = context.get("arp." + Process.EXIT_CODE);
+      assertNotNull(exitCode);
+      assertTrue(exitCode instanceof Integer);
+
+      final Object duration = context.get("arp." + Process.DURATION);
+      assertNotNull(duration);
+
+      final Object output = context.get("arp." + Process.OUTPUT);
+      assertNotNull(output);
+
+      final Object error = context.get("arp." + Process.ERROR);
+      assertNotNull(error);
+    }
+  }
+
+
+
+
+  @Test(expected = TaskException.class)
+  public void haltOnError() throws IOException, ConfigurationException, TaskException {
+    final TransformContext context = new TransformContext();
+
+    final Config cfg = new Config();
+    cfg.put(Process.COMMAND, "foo -baz -bar");
+    cfg.put(ConfigTag.HALT_ON_ERROR, true);
+    Log.info("\"Process\":" + cfg);
+
+    try (Process task = new Process()) {
+      task.setConfiguration(cfg);
+      task.open(context);
+      task.execute();
+    }
+    fail("Should have thrown an exception");
+  }
+
+
+
+
+  @Test
+  public void noCommand() throws IOException, ConfigurationException, TaskException {
+    final TransformContext context = new TransformContext();
+    final Config cfg = new Config();
+    try (Process task = new Process()) {
+      task.setConfiguration(cfg);
+      task.open(context);
+      task.execute();
+    }
+  }
+
+}
