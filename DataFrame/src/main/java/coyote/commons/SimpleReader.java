ba8eea4406556f5568910e257a0ab97c6ec232ed
Steve Cote <sdcote@gmail.com>
2017-09-07 13:48:02 -0400

    Javadoc preamble update

--- DataFrame/src/main/java/coyote/commons/SimpleReader.java
+++ DataFrame/src/main/java/coyote/commons/SimpleReader.java
@@ -4,10 +4,6 @@
  * This program and the accompanying materials are made available under the 
  * terms of the MIT License which accompanies this distribution, and is 
  * available at http://creativecommons.org/licenses/MIT/
- *
- * Contributors:
- *   Stephan D. Cote 
- *      - Initial API and implementation
  */
 package coyote.commons;
 
