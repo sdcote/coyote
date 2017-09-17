2208c4c3aabaaee7af4e1a18f86bc90e88300e50
Steve Cote <sdcote@gmail.com>
2017-09-16 21:05:50 -0400

    Timeout configuration tag added

--- CoyoteDX/src/main/java/coyote/dx/ConfigTag.java
+++ CoyoteDX/src/main/java/coyote/dx/ConfigTag.java
@@ -119,6 +119,7 @@ public class ConfigTag {
   public static final String HOST = "hostname";
   public static final String PORT = "port";
   public static final String PROTOCOL = "protocol";
+  public static final String TIMEOUT = "timeout";
   public static final String FILE = "filename";
   public static final String DIRECTORY = "directory";
   public static final String APPEND = "append";
