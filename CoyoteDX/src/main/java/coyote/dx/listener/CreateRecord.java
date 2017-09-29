888bf818b0ab2f4b06a7a5a34dc4d2f854d0fbbb
Steve Cote <sdcote@gmail.com>
2017-09-29 09:20:21 -0400

    Added simple storage format

--- CoyoteDX/src/main/java/coyote/dx/listener/CreateRecord.java
+++ CoyoteDX/src/main/java/coyote/dx/listener/CreateRecord.java
@@ -10,6 +10,7 @@ package coyote.dx.listener;
 import coyote.commons.GUID;
 import coyote.dataframe.DataField;
 import coyote.dataframe.DataFrame;
+import coyote.dataframe.DataFrameException;
 import coyote.dx.CDX;
 import coyote.dx.context.ContextListener;
 import coyote.dx.context.OperationalContext;
@@ -35,12 +36,27 @@ import coyote.loader.log.LogMsg;
  * other components a chance to process the working frame and the mapper to 
  * generate a properly formatted record for insertion into the database.
  * 
+ * <p>This has 2 modes of operation. Simple, the fastest, just serializes the 
+ * data frame as a JSON string and places it in a row. The other mode (not 
+ * simple?) saves each field in a separate row, preserving order and 
+ * hierarchy. This is slower as it generates a batch of inserts for each of 
+ * the fields and then sends the batch to the database. Simple mode can be 
+ * used for 90% of applications since this type of data transfer is normally 
+ * the first stage of integration. THe "non-simple" mode is only used when the 
+ * data frames are stored in their final location and there is a need to edit 
+ * fields of the frame individually.  
+ *  
  * <p>The structure of the table is a normalized attribute table. This allows 
  * any structure to be modeled and enables the table to support different 
  * record types. Each key-value pair is assigned a parent to which it belongs.
  * All pairs should have a parent as all attributes belong to a data frame.
  */
 public class CreateRecord extends AbstractDatabaseListener implements ContextListener {
+
+  private static final String SIMPLE_MODE = "SimpleMode";
+
+
+
 
   /**
    * @see coyote.dx.listener.AbstractListener#onEnd(coyote.dx.context.OperationalContext)
@@ -78,9 +94,12 @@ public class CreateRecord extends AbstractDatabaseListener implements ContextLis
   private void performCreate(TransactionContext cntxt) {
     Log.info("Create Record Listener handling target frame of " + cntxt.getTargetFrame());
     DataFrame frame = cntxt.getWorkingFrame();
-    String guid = GUID.randomGUID().toString();
-    saveFrame(frame, guid, null);
-    // place the GUID in the transaction context so the data frame can be retrieved later
+    String guid;
+    if (isSimpleMode()) {
+      guid = saveEntireFrame(frame);
+    } else {
+      guid = saveFrame(frame, 0, null);
+    }
     cntxt.setProcessingResult(guid);
   }
 
@@ -88,19 +107,65 @@ public class CreateRecord extends AbstractDatabaseListener implements ContextLis
 
 
   /**
+   * This saves the entire frame as a JSON string in the value field.
+   * 
+   * @param frame the frame to save.
+   * 
+   * @return the system identifier for the inserted record.
+   */
+  private String saveEntireFrame(DataFrame frame) {
+    String sysid = GUID.randomGUID().toString();
+    Log.info("Frame Id:" + sysid + " - " + frame);
+    return sysid;
+  }
+
+
+
+
+  /**
+   * @return true if this is to serialize the entire frame into the value 
+   *         field, false to store each field in a separate row.
+   */
+  private boolean isSimpleMode() {
+    boolean retval = true;
+    try {
+      retval = configuration.getAsBoolean(SIMPLE_MODE);
+    } catch (final DataFrameException ignore) {
+      // must not be set or is invalid boolean value
+    }
+
+    return retval;
+  }
+
+  //
+
+  // This is the complicated way to save dataframes
+
+
+
+
+  //
+
+  /**
    * @param frame
    */
-  private void saveFrame(DataFrame frame, String sysid, String parent) {
+  private String saveFrame(DataFrame frame, int num, String parent) {
+    String sysid = null;
     if (frame != null) {
+      sysid = GUID.randomGUID().toString();
+      Log.info("Frame  " + num + " type:FRM parent:" + parent + " SysId:" + sysid);
+
+      int seq = 0;
       for (DataField field : frame.getFields()) {
+
         if (field.isFrame()) {
-          String guid = GUID.randomGUID().toString();
-          saveFrame((DataFrame)field.getObjectValue(), guid, sysid);
+          saveFrame((DataFrame)field.getObjectValue(), seq++, sysid);
         } else {
-          saveField(field, sysid);
+          saveField(field, seq++, sysid);
         }
       }
     }
+    return sysid;
   }
 
 
@@ -109,8 +174,9 @@ public class CreateRecord extends AbstractDatabaseListener implements ContextLis
   /**
    * @param field
    */
-  private void saveField(DataField field, String parent) {
-    Log.info("Saving field '" + field.getName() + "' type:" + field.getTypeName() + " Frame:" + parent);
+  private void saveField(DataField field, int seq, String parent) {
+    String sysid = GUID.randomGUID().toString();
+    Log.info("Field " + seq + " '" + field.getName() + "' type:" + field.getTypeName() + " Frame:" + parent + " SysId:" + sysid);
   }
 
 }
