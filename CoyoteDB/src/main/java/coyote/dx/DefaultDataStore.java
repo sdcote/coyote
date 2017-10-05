4de3970d286cc4b46cd23ac71373b0429e6f2196
Steve Cote <sdcote@gmail.com>
2017-10-05 07:59:28 -0400

    Initial design of shared datastore configuration

---
+++ CoyoteDB/src/main/java/coyote/dx/DefaultDataStore.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ */
+package coyote.dx;
+
+/**
+ * This is a very simple component to create and close JDBC connections.
+ * 
+ * <p>This is a wrapper around a Database object which allows it to be loaded 
+ * in a configuration and accessed by other components in the transform.
+ */
+public class DefaultDataStore extends AbstractDataStore implements DataStore {
+
+}
