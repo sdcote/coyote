0f13c0f1481ee5d85e66ab5993b0a3f07df8a219
Steve Cote <sdcote@gmail.com>
2017-10-16 15:35:30 -0400

    Clean up database files after test

--- CoyoteDB/src/test/java/coyote/H2Driver.java
+++ CoyoteDB/src/test/java/coyote/H2Driver.java
@@ -10,16 +10,54 @@ package coyote;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import coyote.loader.log.ConsoleAppender;
+import coyote.loader.log.Log;
 
 
 /**
  * 
  */
 public class H2Driver {
+  private static final String CATALOG = "h2test";
+
+
+
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    // Log.addLogger(Log.DEFAULT_LOGGER_NAME, new ConsoleAppender(Log.TRACE_EVENTS | Log.DEBUG_EVENTS | Log.INFO_EVENTS | Log.NOTICE_EVENTS | Log.WARN_EVENTS | Log.ERROR_EVENTS | Log.FATAL_EVENTS));
+    Log.addLogger(Log.DEFAULT_LOGGER_NAME, new ConsoleAppender(Log.INFO_EVENTS | Log.WARN_EVENTS | Log.ERROR_EVENTS | Log.FATAL_EVENTS));
+  }
+
+
+
+
+  /**
+   * @throws Exception
+   */
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+    File dbfile = new File(CATALOG + ".mv.db");
+    dbfile.delete();
+    dbfile = new File(CATALOG + ".trace.db");
+    if (dbfile.exists()) {
+      dbfile.delete();
+    }
+  }
+
+
+
 
   @Test
   public void h2OnClassPath() {
@@ -31,7 +69,7 @@ public class H2Driver {
     }
     Connection connection = null;
     try {
-      connection = DriverManager.getConnection("jdbc:h2:./test;MODE=Oracle", "sa", "");
+      connection = DriverManager.getConnection("jdbc:h2:./" + CATALOG + ";MODE=Oracle", "sa", "");
     } catch (Exception e) {
       e.printStackTrace();
       fail("Connection Failed! " + e.getClass().getName() + " - " + e.getMessage());
