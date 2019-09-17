7aa1e58f76db300be2f9de36016808e54efa79f3
sdcote <sdcote@aep.com>
2019-09-17 17:28:48 -0400

    Incidents now support current sprint defining the concept of new incidents.

--- CoyoteMC/src/main/java/coyote/dx/reader/SnowIncidentMetricReader.java
+++ CoyoteMC/src/main/java/coyote/dx/reader/SnowIncidentMetricReader.java
@@ -1,6 +1,7 @@
 package coyote.dx.reader;
 
 import coyote.commons.StringUtil;
+import coyote.commons.network.http.Method;
 import coyote.dataframe.DataFrame;
 import coyote.dx.CMC;
 import coyote.dx.ConfigTag;
@@ -8,16 +9,10 @@ import coyote.dx.FrameReader;
 import coyote.dx.context.TransactionContext;
 import coyote.dx.context.TransformContext;
 import coyote.loader.log.Log;
-import coyote.mc.snow.ServiceNow;
-import coyote.mc.snow.SnowException;
-import coyote.mc.snow.SnowFilter;
-import coyote.mc.snow.SnowIncident;
+import coyote.mc.snow.*;
 
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static coyote.mc.snow.Predicate.IS;
 import static coyote.mc.snow.Predicate.LIKE;
@@ -27,9 +22,37 @@ import static coyote.mc.snow.Predicate.LIKE;
  * on the incidents in the "incident" table.
  *
  * <p>This counts the current number of open incidents</p>
+ *
+ * <p>An example configuration would look like this:<pre>
+ * "Reader" : {
+ *   "class" : "SnowIncidentMetricReader",
+ *   "source" : "https://myinstance.service-now.com/",
+ *   "product": "Enterprise Widget Framework",
+ *   "ConfigurationItem": "thingy",
+ *   "instance": "widgets",
+ *   "authenticator": {
+ *     "class" : "BasicAuthentication",
+ *     "ENC:username" : "DAGYBWbVRCN0pE0zSb2fqQXizPzKqmV4",
+ *     "ENC:password" : "v5nN0Peh0UO/dcwkKXvYrFrtQn/BSfnl",
+ *     "preemptive" : true
+ *   }
+ * },
+ * </pre>
+ * <p>In the above the optional {@code product} configuration element is used to query the releases and sprint tables
+ * for that product to determine the current sprint. The start date-time is used in determining what is considered a
+ * "new" incident. If no product is specified, then the default sliding window of two weeks is used. If the product,
+ * its releases, or any otherwise active sprint is found, all incidents will appear new and the new count will equal
+ * the total count.</p>
+ *
+ * <p>The {@code configurationitem} element is used to query the symptom_ci for the incidents.</p>
+ *
+ * <p>The {@code instance} element is used to specify the instance name grouping key for the metrics.</p>
  */
 public class SnowIncidentMetricReader extends SnowMetricReader implements FrameReader {
-
+  private static final long TWO_WEEKS = 1000 * 60 * 60 * 24 * 14; // in milliseconds
+  private static final String NEW_ACTIVE_INCIDENT_COUNT = "incident_active_new_count";
+  private static final String ACTIVE_INCIDENT_COUNT = "incident_active_count";
+  SnowDateTime sprintStart = null;
   private List<SnowIncident> incidents = null;
   private String instanceName = null;
 
@@ -49,21 +72,17 @@ public class SnowIncidentMetricReader extends SnowMetricReader implements FrameR
     }
 
     if (context.isNotInError()) {
-      if( filter == null){
+      if (filter == null) {
         String configurationItem = getConfiguration().getString(CONFIG_ITEM);
         if (StringUtil.isBlank(configurationItem)) {
-          context.setError("The " + getClass().getSimpleName() + " configuration did not contain the '" + CONFIG_ITEM + "' or '"+ConfigTag.FILTER+"' element");
+          context.setError("The " + getClass().getSimpleName() + " configuration did not contain the '" + CONFIG_ITEM + "' or '" + ConfigTag.FILTER + "' element");
           context.setState("Configuration Error");
           return;
         }
         filter = new SnowFilter("cmdb_ci", LIKE, configurationItem).and("active", IS, "true");
       }
 
-      try {
-        getResource().setPath("incident_list.do?JSONv2&sysparm_query=" + filter.toEncodedString());
-      } catch (URISyntaxException e) {
-        e.printStackTrace();
-      }
+      getResource().getDefaultParameters().setMethod(Method.GET);
     } // context not in error
   }
 
@@ -76,6 +95,29 @@ public class SnowIncidentMetricReader extends SnowMetricReader implements FrameR
   @Override
   public DataFrame read(TransactionContext context) {
     if (incidents == null) {
+
+      if (StringUtil.isNotBlank(getConfiguration().getString(PRODUCT))) {
+        List<SnowSprint> sprints = getSprints(getConfiguration().getString(PRODUCT));
+        for (SnowSprint sprint : sprints) {
+          if (sprint.isCurrent()) {
+            Log.notice("Current sprint " + sprint.getShortDescription() + " (" + sprint.getScheduledStartDate() + " - " + sprint.getScheduledEndDate() + ")");
+            sprintStart = sprint.getScheduledStartDate();
+            break;
+          }
+        }
+      } else {
+        Log.notice("No product was specified, using the default interval for 'new' incidents.");
+      }
+
+      if (sprintStart == null) sprintStart = new SnowDateTime(new Date(getContext().getStartTime() - TWO_WEEKS));
+
+      try {
+        getResource().setPath("incident_list.do?JSONv2&sysparm_query=" + filter.toEncodedString());
+      } catch (URISyntaxException e) {
+        e.printStackTrace();
+      }
+
+
       incidents = new ArrayList<>();
       for (DataFrame frame = super.read(context); frame != null; frame = super.read(context)) {
         try {
@@ -94,13 +136,20 @@ public class SnowIncidentMetricReader extends SnowMetricReader implements FrameR
 
   private List<DataFrame> generateMetrics(List<SnowIncident> incidents) {
     List<DataFrame> metrics = new ArrayList<>();
+    metrics.addAll(generateNewIncidentCounts(incidents));
+    metrics.addAll(generateIncidentTypeCounts(incidents));
+    return metrics;
+  }
+
+  private List<DataFrame> generateIncidentTypeCounts(List<SnowIncident> incidents) {
+    List<DataFrame> metrics = new ArrayList<>();
     Map<String, Integer> counts = new HashMap<>();
-    for(int i =1; i<=5; i++){
-      counts.put(ServiceNow.getPriorityValue(Integer.toString(i)).toLowerCase(),0);
+    for (int i = 1; i <= 5; i++) {
+      counts.put(ServiceNow.getPriorityValue(Integer.toString(i)).toLowerCase(), 0);
     }
-    int total =0;
+    int total = 0;
     for (SnowIncident incident : incidents) {
-      if( incident.isActive()) {
+      if (incident.isActive()) {
         total++;
         String priority = ServiceNow.getPriorityValue(incident.getPriority()).toLowerCase();
         if (counts.get(priority) != null) {
@@ -112,15 +161,40 @@ public class SnowIncidentMetricReader extends SnowMetricReader implements FrameR
     }
     for (Map.Entry<String, Integer> entry : counts.entrySet()) {
       DataFrame metric = new DataFrame();
-      metric.set(ConfigTag.NAME, "incident_count_"+entry.getKey());
+      metric.set(ConfigTag.NAME, "incident_" + entry.getKey() + "_count");
       metric.set(ConfigTag.VALUE, entry.getValue());
       metric.set(ConfigTag.HELP, "The number of active incidents items with the priority of '" + entry.getKey() + "'");
       metric.set(ConfigTag.TYPE, CMC.GAUGE);
       metric.set(INSTANCE, instanceName);
       metrics.add(metric);
     }
+    DataFrame metric = new DataFrame();
+    metric.set(ConfigTag.NAME, ACTIVE_INCIDENT_COUNT);
+    metric.set(ConfigTag.VALUE, total);
+    metric.set(ConfigTag.HELP, "The number of active incidents");
+    metric.set(ConfigTag.TYPE, CMC.GAUGE);
+    metric.set(INSTANCE, instanceName);
+    metrics.add(metric);
+
     return metrics;
   }
+
+  private List<DataFrame> generateNewIncidentCounts(List<SnowIncident> incidents) {
+    List<DataFrame> metrics = new ArrayList<>();
+    int activeCount = 0;
+    for (SnowIncident incident : incidents) {
+      if (incident.isActive() && incident.getCreatedTimestamp().compareTo(sprintStart) > 0) activeCount++;
+    }
+    DataFrame metric = new DataFrame();
+    metric.set(ConfigTag.NAME, NEW_ACTIVE_INCIDENT_COUNT);
+    metric.set(ConfigTag.VALUE, activeCount);
+    metric.set(ConfigTag.HELP, "The number of new active incidents");
+    metric.set(ConfigTag.TYPE, CMC.GAUGE);
+    metric.set(INSTANCE, instanceName);
+    metrics.add(metric);
+    return metrics;
+  }
+
 
 }
   
