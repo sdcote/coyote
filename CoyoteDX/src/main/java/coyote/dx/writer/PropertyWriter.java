b09961e0aea667f681a8d892f163ca8d4e4b265f
sdcote <sdcote@aep.com>
2019-09-18 13:56:27 -0400

    initial functionality

--- CoyoteDX/src/main/java/coyote/dx/writer/PropertyWriter.java
+++ CoyoteDX/src/main/java/coyote/dx/writer/PropertyWriter.java
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2015 Stephan D. Cote' - All rights reserved.
- * 
- * This program and the accompanying materials are made available under the 
- * terms of the MIT License which accompanies this distribution, and is 
+ * Copyright (c) 2019 Stephan D. Cote' - All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License which accompanies this distribution, and is
  * available at http://creativecommons.org/licenses/MIT/
  */
 package coyote.dx.writer;
 
 import coyote.commons.StringUtil;
+import coyote.dataframe.DataField;
 import coyote.dataframe.DataFrame;
-import coyote.dataframe.marshal.JSONMarshaler;
 import coyote.dx.CDX;
 import coyote.dx.ConfigurableComponent;
 import coyote.dx.FrameWriter;
@@ -19,8 +19,14 @@ import coyote.loader.log.LogMsg;
 
 /**
  * Writes a data frame as a simple name-value pair string to either standard output (default) or standard error.
+ *
+ * <p>The name of the pair will be the value of the "name" field in the given dataframe and the value of the pair is
+ * the value of the "value" field. In simpler terms, the given frame must contain a "name" field and a "value" field.
+ * This implies that each received dataframe will result in the creation of one key-value pair.</p>
  */
 public class PropertyWriter extends AbstractFrameFileWriter implements FrameWriter, ConfigurableComponent {
+  private static final String NAME = "name";
+  private static final String VALUE = "value";
 
   /**
    * @see AbstractFrameFileWriter#write(DataFrame)
@@ -47,22 +53,25 @@ public class PropertyWriter extends AbstractFrameFileWriter implements FrameWrit
   }
 
 
-
-
   /**
    * This is where we actually write the frame.
-   * 
+   *
    * @param frame the frame to be written
    */
   private void writeFrame(final DataFrame frame) {
-
-    printwriter.write(JSONMarshaler.toFormattedString(frame));
-    printwriter.write(StringUtil.LINE_FEED);
-    printwriter.flush();
-
+    DataField nameField = frame.getFieldIgnoreCase(NAME);
+    DataField valueField = frame.getFieldIgnoreCase(VALUE);
+    if (nameField != null && valueField != null) {
+      printwriter.write(nameField.getStringValue());
+      printwriter.write(" = ");
+      printwriter.write(valueField.getStringValue());
+      printwriter.write(StringUtil.LINE_FEED);
+      printwriter.flush();
+    } else {
+      Log.warn("did not write property, both 'name' and 'value' field are required:\nName: " + nameField + "\nValue:" + valueField);
+    }
     // Increment the row number
     rowNumber++;
-
   }
 
 }
