658b5549a4aba894c20a512e3b3d612193475907
Steve Cote <sdcote@gmail.com>
2019-09-17 21:29:02 -0400

    Added copyright preamble

--- CoyoteMC/src/main/java/coyote/dx/reader/WebPageMetricReader.java
+++ CoyoteMC/src/main/java/coyote/dx/reader/WebPageMetricReader.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2019 Stephan D. Cote' - All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License which accompanies this distribution, and is
+ * available at http://creativecommons.org/licenses/MIT/
+ */
+
 package coyote.dx.reader;
 
 import coyote.dataframe.DataFrame;
