0bc7d307d3315e2e2994f80e817010cc97895285
Steve Cote <sdcote@gmail.com>
2019-09-19 08:51:57 -0400

    added default  secrets access method - get(String)

--- CoyoteDX/src/main/java/coyote/dx/vault/VaultProxy.java
+++ CoyoteDX/src/main/java/coyote/dx/vault/VaultProxy.java
@@ -21,7 +21,24 @@ public class VaultProxy {
   }
 
   /**
-   * Retreive the vault entry with the given identifier and return the value of the named vault entry element.
+   * Retrieve the vault entry with the given identifier and return the default vault entry element.
+   *
+   * <p>For most implementations this will be the password. But, there are other implementation which are simply
+   * "secrets" vaults and the identifier is simply the identifier of the secret. For example, CyberArk only returns a
+   * "content" element.</p>
+   *
+   * <p>Implementers of this method are encourages to return the password element as a matter of convention, allowing
+   * more granular access via {@link #get(String, String)} method call.</p>
+   *
+   * @param entryId the identifier for the vault entry
+   * @return the identified element value or an empty string if not found or if the element is empty. Will never return null.
+   */
+  public String get(String entryId) {
+    return "";
+  }
+
+  /**
+   * Retrieve the vault entry with the given identifier and return the value of the named vault entry element.
    *
    * @param entryId      the identifier for the vault entry
    * @param entryElement the name of the entry element to return.
