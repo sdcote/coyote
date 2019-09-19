6842d100bfe1bd8496c5efb982220fdbe29227f1
sdcote <sdcote@aep.com>
2019-09-19 16:54:30 -0400

    Fixed locating lib directory

--- CoyoteSV/src/dist/bin/csv.bat
+++ CoyoteSV/src/dist/bin/csv.bat
@@ -1,3 +1,6 @@
-@echo off
+@if "%DEBUG%" == "" @echo off
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%..
 start /B javaw -jar %APP_HOME%\lib\CoyoteVault-0.0.1.jar %*
