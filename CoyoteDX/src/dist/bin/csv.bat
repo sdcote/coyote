869c673622f05d8724e47947552771e050c2bb64
Steve Cote <sdcote@gmail.com>
2019-09-22 22:17:01 -0400

    Use the latest version of the vault

--- CoyoteDX/src/dist/bin/csv.bat
+++ CoyoteDX/src/dist/bin/csv.bat
@@ -3,4 +3,4 @@ set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%..
-start /B javaw -jar %APP_HOME%\lib\CoyoteVault-0.0.1.jar %*
+start /B javaw -jar %APP_HOME%\lib\CoyoteVault-0.0.2.jar %*
--- CoyoteDX/src/dist/bin/csv.sh
+++ CoyoteDX/src/dist/bin/csv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 APP_HOME="`pwd -P`"
-java -jar $APP_HOME/lib/CoyoteVault-0.0.1.jar "$@"
+java -jar $APP_HOME/lib/CoyoteVault-0.0.2.jar "$@"
