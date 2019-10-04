297def5727a906d9becaac30a53921d04bec1626
sdcote <sdcote@aep.com>
2019-10-04 10:16:58 -0400

    Json is now correctly structured as an array of objects.

--- CoyoteDX/src/main/java/coyote/dx/writer/JsonWriter.java
+++ CoyoteDX/src/main/java/coyote/dx/writer/JsonWriter.java
@@ -1,13 +1,12 @@
 /*
  * Copyright (c) 2015 Stephan D. Cote' - All rights reserved.
- * 
- * This program and the accompanying materials are made available under the 
- * terms of the MIT License which accompanies this distribution, and is 
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License which accompanies this distribution, and is
  * available at http://creativecommons.org/licenses/MIT/
  */
 package coyote.dx.writer;
 
-import coyote.commons.StringUtil;
 import coyote.dataframe.DataFrame;
 import coyote.dataframe.marshal.JSONMarshaler;
 import coyote.dx.CDX;
@@ -18,8 +17,10 @@ import coyote.loader.log.LogMsg;
 
 
 /**
- * Writes a data frame as a simple JSON string to either standard output 
+ * Writes a data frame as a simple JSON string to either standard output
  * (default) or standard error.
+ *
+ * <p>All data is formatted as an array of JSON objects.</p>
  */
 public class JsonWriter extends AbstractFrameFileWriter implements FrameWriter, ConfigurableComponent {
 
@@ -48,22 +49,23 @@ public class JsonWriter extends AbstractFrameFileWriter implements FrameWriter, 
   }
 
 
-
-
   /**
    * This is where we actually write the frame.
-   * 
+   *
    * @param frame the frame to be written
    */
   private void writeFrame(final DataFrame frame) {
-
+    if (rowNumber == 0) {
+      printwriter.write('[');
+    }
     printwriter.write(JSONMarshaler.toFormattedString(frame));
-    printwriter.write(StringUtil.LINE_FEED);
+    if (getContext().getTransaction().isLastFrame()) {
+      printwriter.write(']');
+    } else {
+      printwriter.write(',');
+    }
     printwriter.flush();
-
-    // Increment the row number
     rowNumber++;
-
   }
 
 }
